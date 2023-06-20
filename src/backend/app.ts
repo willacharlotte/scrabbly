3114df84b29eed21000f7f74e23ccd5d3d355859
willa <willa@indiefin.com>
2023-06-20 18:11:33 +0200

    feat: init app with 2 endpoints

--- src/backend/app.ts
+++ src/backend/app.ts
@@ -1,2 +1,14 @@
-import express from 'express'
+import express from "express";
+import { getSquares, getTiles } from "./controllers";
 
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+app.get("/squares", getSquares);
+app.get("/tiles", getTiles);
+
+app.listen(port, () => {
+  console.log(`app listening on ${port}`);
+});
