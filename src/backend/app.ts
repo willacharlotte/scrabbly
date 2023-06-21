e3c53773fdcc16d6d0e32d0b3e95ef32a6d7299b
Gabriel Tidy <gabriel@bbd.co.za>
2023-06-21 12:26:44 +0200

    added TypeScript to the dependencies, added routing for the frontend

--- src/backend/app.ts
+++ src/backend/app.ts
@@ -2,9 +2,17 @@ import express from "express";
 import { getSquares, getTiles } from "./controllers";
 
 const app = express();
-const port = 3000;
+const port = 4000;
 
 app.use(express.json());
+
+//all html pages, stylesheets and scripts from the frontend
+app.use(express.static('./src/frontend', {extensions:['html']}));
+
+//root page
+app.get('/', function(_, res){
+  res.sendFile('index.html', { root: './src/frontend/html' , extensions:['html'] });
+});
 
 app.get("/squares", getSquares);
 app.get("/tiles", getTiles);
