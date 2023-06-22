1370914d4b1d7fec1964b76eccaa25d5f19f0d06
willa <willa@indiefin.com>
2023-06-22 14:44:04 +0200

    feat: add users dal

---
+++ src/backend/dal/users.ts
@@ -0,0 +1,56 @@
+import { User } from "../types";
+
+export namespace Users {
+  // TODO put this in db
+  const users: User[] = [];
+
+  export const getUsers = () => {
+    return users;
+  };
+
+  export const getUserByUsername = (username: string) => {
+    const user = users.find((user) => user.username === username);
+    if (user === undefined)
+      throw new Error("user with specified username does not exist");
+
+    return user;
+  };
+
+  export const doesUsernameExist = (username: string) => {
+    try {
+      getUserByUsername(username);
+      return true;
+    } catch {
+      return false;
+    }
+  };
+
+  export const postNewUser = (username: string) => {
+    if (doesUsernameExist(username))
+      throw new Error("user with specified username already exists");
+
+    const newUser: User = {
+      username: username,
+      games: [],
+    };
+
+    users.push(newUser);
+    return newUser;
+  };
+
+  export const putUser = (userToPut: User) => {
+    const user = getUserByUsername(userToPut.username);
+    user.games = userToPut.games;
+    return true;
+  };
+
+  export const deleteUser = (userToDel: User) => {
+    const indexOfUser = users.findIndex(
+      (user) => user.username === userToDel.username
+    );
+    if (indexOfUser === -1) throw new Error("user does not exist");
+
+    users.splice(indexOfUser, 1);
+    return true;
+  };
+}
