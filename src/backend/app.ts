deaaf517eda68ce52bf2de1689a199ba71577ead
willa <willa@indiefin.com>
2023-06-22 16:41:16 +0200

    feat: game controllers and endpoints

--- src/backend/app.ts
+++ src/backend/app.ts
@@ -1,5 +1,13 @@
 import express from "express";
-import { getSquares, getTiles } from "./controllers";
+import {
+  getSquares,
+  getTiles,
+  getGame,
+  getGames,
+  newGame,
+  putMove,
+  deleteGame,
+} from "./controllers";
 
 const app = express();
 const port = 4000;
@@ -7,21 +15,32 @@ const port = 4000;
 app.use(express.json());
 
 //middleware to log all methods to console - only for testing purposes
-app.use('*', (req, _, next) => {
+app.use("*", (req, _, next) => {
   console.log(`${req.method} on ${req.originalUrl}`);
   next();
 });
 
 //all html pages, stylesheets and scripts from the frontend
-app.use(express.static('./src/frontend', {extensions:['html']}));
+app.use(express.static("./src/frontend", { extensions: ["html"] }));
 
 //root page
-app.get('/', function(_, res){
-  res.sendFile('index.html', { root: './src/frontend/html' , extensions:['html'] });
+app.get("/", function (_, res) {
+  res.sendFile("index.html", {
+    root: "./src/frontend/html",
+    extensions: ["html"],
+  });
 });
+
+// backend methods
 
 app.get("/squares", getSquares);
 app.get("/tiles", getTiles);
+
+app.get("/games", getGames);
+app.get("/games/:id", getGame);
+app.post("/games", newGame);
+app.put("/games/:id/move", putMove);
+app.delete("/games/:id", deleteGame);
 
 app.listen(port, () => {
   console.log(`app listening on ${port}`);
--- src/backend/controllers/index.ts
+++ src/backend/controllers/index.ts
@@ -1,2 +1,3 @@
 export { getSquares } from "./get-squares";
 export { getTiles } from "./get-tiles";
+export { getGames, getGame, newGame, putMove, deleteGame } from "./games";
