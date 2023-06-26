89e9f962a55a5539cdb3e2e5b2b5c82c7101a0a4
willa <willa@indiefin.com>
2023-06-26 17:42:14 +0200

    feat: new ENV utility with a dotenv dependency

--- src/backend/utils/index.ts
+++ src/backend/utils/index.ts
@@ -3,3 +3,4 @@ export { fillRack } from "./fill-rack";
 export { createNewGame } from "./create-new-game";
 export { newPlacedTilesFromMove } from "./new-placed-tiles-from-move";
 export { updateRack } from "./update-rack";
+export { ENV } from "./dot-env";
