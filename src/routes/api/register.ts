97548ec2c7535f15b3c7ba8da3d14c963e86a324
mmasehume-bbd <122437638+mmasehume-bbd@users.noreply.github.com>
2023-06-27 17:15:01 +0200

    Revert "Feature/identity server"

--- src/routes/api/register.ts
+++
@@ -1,6 +0,0 @@
-import express from "express";
-import createCredentialsHandler from "../../identityServer/handlers/register";
-
-export const registerRoute = express.Router();
-
-registerRoute.post("/create-credentials", createCredentialsHandler);
