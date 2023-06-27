3a98654ee3f53bca2d15fed9eafd08c5e0a7b19e
willa <willa@indiefin.com>
2023-06-27 15:43:00 +0200

    feat: add login page

---
+++ src/frontend/js/login.js
@@ -0,0 +1,11 @@
+const loginForm = document.getElementById("login-form");
+
+loginForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+});
+
+const registerForm = document.getElementById("register-form");
+
+registerForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+});
