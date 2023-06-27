6742f252d0824853d4162add95eee72846c90b8b
Mmasehume Raphiri <mmasehume@bbd.co.za>
2023-06-27 17:52:11 +0200

    sever

---
+++ identityServer/server.ts
@@ -0,0 +1,57 @@
+// import ttl from "level-ttl";
+import express from "express";
+import { Level } from "level";
+import cookieParser from 'cookie-parser';
+
+import { registerRoute } from "../src/routes/api/register";
+import { exchangeCredentialsRoute } from "../src/routes/api/credentials";
+import { authenticationRoute } from "../src/routes/api/authentication";
+import { usersRoute } from "../src/routes/api/users";
+
+import verifyToken from "./middleware/verify-token";
+import { boardRoute } from "../src/routes/api/board";
+import { gamesRoute } from "../src/routes/api/games";
+// import { credentials } from "../backend/service/middleware/credentials";
+import { DbConnection } from "../src/backend/dal/db-connection";
+
+const app = express();
+const port = 8080;
+
+DbConnection.connectDB();
+
+// handle credentials before cors
+// app.use(credentials);
+
+// middleware for cookies
+app.use(cookieParser());
+
+// serve static files
+
+// register
+app.use('/', registerRoute)
+// login
+app.use('/', exchangeCredentialsRoute)
+// auth
+app.use('/', authenticationRoute)
+
+// app.use(express.json());
+app.locals.store = new Level('./store', { valueEncoding: 'json' })
+// app.locals.ttl = ttl(app.locals.store);
+
+// backend routes
+app.use(verifyToken); //all routes after this are secured by middleware
+app.use('/', usersRoute);
+app.use('/', gamesRoute);
+app.use('/', boardRoute);
+
+app.get("/test", async (_, res) => {
+  res.end(
+    JSON.stringify(await DbConnection.runQuery(`SELECT * FROM dbo.Player`))
+  );
+});
+
+
+// need to check if db connection is open before checking for requests
+app.listen(port, () => {
+    console.log(`app listening on ${port}`);
+  });
--- src/routes/api/authentication.ts
+++ src/routes/api/authentication.ts
@@ -2,5 +2,4 @@ import express from "express";
 import bearer from "../../identityServer/middleware/verify-token";
 
 export const authenticationRoute = express.Router();
-
 authenticationRoute.get("/check-authentication", bearer, (request: express.Request, response: express.Response) => response.send("`You're authenticated!"));
