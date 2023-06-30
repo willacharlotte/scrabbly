cdcf41c4f5a35a436c0a2671cc1c1274ec7ab9a7
willa <willa@indiefin.com>
2023-06-30 14:23:30 +0200

    feat: add password validation to login

--- resource-server/src/frontend/js/login.js
+++ resource-server/src/frontend/js/login.js
@@ -1,7 +1,32 @@
 const password = document.getElementById("register-password");
 const passwordVerify = document.getElementById("register-password-verify");
 
+const checkPassword = (pass) => {
+  if (pass.length < 8) return false;
+
+  const numbers = /[0-9]/g;
+  const lowerLetters = /[a-z]/g;
+  const upperLetters = /[A-Z]/g;
+
+  const hasNumbers = pass.search(numbers) > -1;
+  const hasLowerLetters = pass.search(lowerLetters) > -1;
+  const hasUpperLetters = pass.search(upperLetters) > -1;
+  const hasSymbols =
+    pass
+      .replace(numbers, "")
+      .replace(lowerLetters, "")
+      .replace(upperLetters, "") > 0;
+
+  return hasNumbers + hasLowerLetters + hasUpperLetters + hasSymbols >= 3;
+};
+
 const validatePassword = () => {
+  if (checkPassword(password.value))
+    passwordVerify.setCustomValidity(
+      "Password length should be greater than 8 " +
+        "and contain at least 3 of the following 4 sets: " +
+        "numbers, uppercase letters, lowercase letters and symbols"
+    );
   if (password.value != passwordVerify.value)
     passwordVerify.setCustomValidity("Passwords don't match");
   else passwordVerify.setCustomValidity("");
